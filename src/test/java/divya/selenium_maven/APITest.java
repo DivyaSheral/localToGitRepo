36aeee7e0efcb5b5b57b751b75e49c43ef8b5175
Divya <dvdasari91@gmail.com>
2023-01-04 15:51:24 -0600

    First commit from local to git repo

---
+++ src/test/java/divya/selenium_maven/APITest.java
@@ -0,0 +1,19 @@
+package divya.selenium_maven;
+
+import org.testng.annotations.Test;
+
+public class APITest {
+	
+	@Test
+	public void loginApi()
+	{
+		System.out.print("login test");
+	}
+	
+	@Test
+	public void homePageApi()
+	{
+		System.out.print("login test");
+	}
+
+}
---
+++ src/test/java/divya/selenium_maven/SeleniumTest.java
@@ -0,0 +1,19 @@
+package divya.selenium_maven;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void login()
+	{
+		System.out.print("login test");
+	}
+	
+	@Test
+	public void homePage()
+	{
+		System.out.print("login test");
+	}
+
+}
